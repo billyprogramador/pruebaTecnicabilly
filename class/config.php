a49f6424b5b78f209bec17b0f1c7468277763ff4
billyprogramador <91508081+billyprogramador@users.noreply.github.com>
2021-09-29 15:30:54 -0500

    cargando carpetas faltantes
    cargue de carpeta css y y bd que no se cargaron correctamente.

---
+++ class/config.php
@@ -0,0 +1,9 @@
+﻿<?php
+/*Datos de Conexion Mysql*/
+$DB_TYPE_MYSQL = 'mysql';
+$DB_HOST_MYSQL = 'localhost';
+$DB_NAME_MYSQL = 'prueba_tecnica_dev';
+$DB_USER_MYSQL = 'root';
+$DB_PASS_MYSQL = "Ar12345*";
+
+?>
